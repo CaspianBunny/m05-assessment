81ecfa215d89f15241665a5caee6b48f764d8836
planierdev10 <planier@dev-10.com>
2024-08-09 16:08:42 -0400

    messar

--- sustainable-foraging/src/test/java/learn/foraging/domain/ForagerServiceTest.java
+++ sustainable-foraging/src/test/java/learn/foraging/domain/ForagerServiceTest.java
@@ -1,2 +1,21 @@
-package learn.foraging.domain;public class ForagerServiceTest {
+package learn.foraging.domain;
+
+import learn.foraging.data.ForagerRepositoryDouble;
+import learn.foraging.models.Forager;
+import org.junit.jupiter.api.Test;
+
+import static learn.foraging.TestHelper.makeResult;
+
+public class ForagerServiceTest {
+
+    ForagerService service = new ForagerService(new ForagerRepositoryDouble());
+
+
+    @Test
+    void shouldAdd() {
+        Forager arg = new Forager(0, "John", "Doe", "KY");
+        Result <Forager> expected = makeResult(null, new Forager(5, "John", "Doe", "KY"));
+        Result <Forager> actual = service.add(arg);
+
+    }
 }
